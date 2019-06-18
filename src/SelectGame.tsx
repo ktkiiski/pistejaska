9e9ab17adc680bcf0889c8f079f4a3a8e0786219
Kimmo Kiiski <kimmo@kii.ski>
2019-06-18 13:30:22 +0300

    Sort games by name

--- src/SelectGame.tsx
+++ src/SelectGame.tsx
@@ -13,6 +13,12 @@ export const SelectGame = (props: RouteComponentProps<{}>) => {
     props.history.push("/new/" + game.id);
 
   const [searchTerm, setSearchTerm] = useState("");
+  const listedGames = games.map((game) => ({
+    ...game, lowercaseName: game.name.toLowerCase(),
+  }));
+  listedGames.sort(({ lowercaseName: name1 }, { lowercaseName: name2 }) => {
+    return name1 > name2 ? 1 : name1 < name2 ? -1 : 0;
+  });
   return (
     <div>
       <h2>Select game</h2>
@@ -23,7 +29,7 @@ export const SelectGame = (props: RouteComponentProps<{}>) => {
         onChange={e => setSearchTerm(e.currentTarget.value)}
       />
       <List component="nav">
-        {games
+        {listedGames
           .filter(g => g.name.includes(searchTerm))
           .map(game => (
             <ListItem button onClick={() => onSelectGame(game)} key={game.id}>
