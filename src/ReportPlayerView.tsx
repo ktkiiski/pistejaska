1b5b0a366786924f335cc4f2235f8ccb98597570
Panu Vuorinen <panu.vuorinen@gmail.com>
2020-10-31 20:32:09 +0200

    Added TrueSkill to player reports

--- src/ReportPlayerView.tsx
+++ src/ReportPlayerView.tsx
@@ -7,6 +7,7 @@ import { sortBy, flatMap, groupBy, uniq } from "lodash";
 import { usePlays } from "./common/hooks/usePlays";
 import ReportTable from "./ReportTable";
 import { stringifyScore } from "./common/stringUtils";
+import { calculateEloForPlayers } from "./domain/ratings";
 
 export const ReportPlayerView = (props: RouteComponentProps<any>) => {
   const playerId = props.match.params["playerId"];
@@ -43,26 +44,30 @@ export const ReportPlayerView = (props: RouteComponentProps<any>) => {
 
       <PlayerGamesReport
         player={player}
-        playerPlays={playerPlays}
+        plays={plays}
       ></PlayerGamesReport>
     </div>
   );
 };
 
-const PlayerGamesReport = (props: { player: Player; playerPlays: Play[] }) => {
-  const { player, playerPlays } = props;
+const PlayerGamesReport = (props: { player: Player; plays: Play[] }) => {
+  const { player, plays } = props;
+  const playerPlays = plays.filter(p => p.players.find(x => x.id === player.id));
   const playerGames = uniq(Object.keys(groupBy(playerPlays, (p) => p.gameId)));
-
+  
   const columns = [
     { name: "Game name" },
     { name: "Max points" },
     { name: "Best position" },
+    { name: "Trueskill", tooltip: "The skill level of player according to TrueSkill algorithm. Lower is better. If 10%, then the player is among the best 10% of players." },
     { name: "Wins" },
     { name: "# of plays" },
   ];
 
   const rows = playerGames.concat(["all"]).map((g) => {
     const gamePlays = g === "all" ? playerPlays : playerPlays.filter((x) => x.gameId === g);
+
+    const allGamePlays = g === "all" ? plays : plays.filter(p => p.gameId === g); 
 
     const maxScoresPlay = sortBy(gamePlays, (p) =>
       p.getTotal(player.id)
@@ -73,6 +78,13 @@ const PlayerGamesReport = (props: { player: Player; playerPlays: Play[] }) => {
     const winnedPlays = gamePlays.filter(p => p.getPosition(player.id) === 1);
 
     const game = games.find((x) => x.id === g);
+
+    const trueSkills = calculateEloForPlayers(allGamePlays, 0);
+
+    const playerTrueSkillIndex = trueSkills.findIndex(x => x.id === player.id);
+
+    const bracket = Math.ceil(Math.round(playerTrueSkillIndex / trueSkills.length * 100)/10)*10+10;
+
     return [
       {
         value: game?.name ?? "All",
@@ -85,6 +97,9 @@ const PlayerGamesReport = (props: { player: Player; playerPlays: Play[] }) => {
       {
         value: bestPositionPlay.getPosition(player.id).toString(),
         link: `/view/${bestPositionPlay.id}`,
+      },
+      {
+        value: `Best ${bracket}%`,
       },
       {
         value: `${Math.round((winnedPlays.length/gamePlays.length*100))}% (${winnedPlays.length})`,
