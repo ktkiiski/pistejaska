a902e279d3688a70a3002b6af5095d8f18154a87
Kimmo Kiiski <kimmo@kii.ski>
2022-01-14 22:02:28 +0200

    Refactor Firestore hooks

--- src/common/hooks/usePlay.ts
+++ src/common/hooks/usePlay.ts
@@ -1,19 +1,9 @@
-import { useCollection } from "react-firebase-hooks/firestore";
-import { getFirestore, collection } from 'firebase/firestore';
-import { Play, PlayDTO } from "../../domain/play";
-import { useMemo } from "react";
-import { app } from "../firebase";
+import { Play } from "../../domain/play";
+import { usePlays } from "./usePlays";
 
 export const usePlay = (
   playId: string
 ): [Play | null, boolean, Error | undefined] => {
-  const [value, loading, error] = useCollection(
-    collection(getFirestore(app), 'plays-v1')
-  );
-  const doc = value && value.docs.find((d) => d.id === playId);
-  const play = useMemo(
-    () => (doc && new Play(doc.data() as PlayDTO)) || null,
-    [doc]
-  );
-  return [play, loading, error];
+  const [plays, loading, error] = usePlays();
+  return [plays.find((play) => play.id === playId) || null, loading, error];
 };
