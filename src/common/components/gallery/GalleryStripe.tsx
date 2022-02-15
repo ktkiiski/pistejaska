c48c9a9d28d7be5fed4bb5cfc1a9cdfaa00c7385
Kimmo Kiiski <kimmo@kii.ski>
2022-02-15 21:33:45 +0200

    Gallery stripe loads images lazily on scroll

--- src/common/components/gallery/GalleryStripe.tsx
+++ src/common/components/gallery/GalleryStripe.tsx
@@ -1,43 +1,99 @@
 import classNames from "classnames";
-import { useState, VFC } from "react";
+import { useCallback, useEffect, useRef, useState, VFC } from "react";
+import { CSSTransition } from "react-transition-group";
 import GalleryOverlay from "./GalleryOverlay";
+import styles from "./GalleryStripe.module.css";
+import { CSSTransitionClassNames } from "react-transition-group/CSSTransition";
 
 interface GalleryStripeProps {
   className?: string;
   images: string[];
 }
 
+const imageTransitionClassNames: CSSTransitionClassNames = {
+  enterActive: styles.entering,
+  enterDone: styles.entered,
+};
+
+const loadThreshold = 100;
+
 const GalleryStripe: VFC<GalleryStripeProps> = ({ className, images }) => {
-  const [fullScreenImageIndex, setFullScreenImageIndex] = useState(0);
-  const [isImageOverlayOpen, setIsImageOverlayOpen] = useState(false);
+  const [visibleCount, setVisibleCount] = useState(0);
+  const [renderCount, setRenderCount] = useState(1);
+  const [imageIndex, setImageIndex] = useState(0);
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
   const [sourceImageRect, setSourceImageRect] = useState<DOMRect | null>(null);
+  const placeholderRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const onImageLoad = useCallback(() => {
+    setVisibleCount((count) => count + 1);
+  }, []);
+
+  const checkLoadMore = useCallback(() => {
+    const container = containerRef.current!;
+    const placeholder = placeholderRef.current;
+    if (placeholder) {
+      const { scrollLeft, clientWidth } = container;
+      const offsetLeft = placeholder?.offsetLeft ?? 0;
+      if (scrollLeft + clientWidth + loadThreshold >= offsetLeft) {
+        setRenderCount(visibleCount + 1);
+      }
+    }
+  }, [visibleCount]);
+
+  useEffect(() => {
+    const placeholder = placeholderRef.current;
+    if (placeholder && visibleCount) {
+      const estimatedImageWidth = placeholder.offsetLeft / visibleCount;
+      const estimatedRemainingWidth =
+        estimatedImageWidth * (images.length - visibleCount);
+      placeholder.style.width = `${Math.ceil(estimatedRemainingWidth)}px`;
+    }
+    checkLoadMore();
+    const container = containerRef.current!;
+    container.addEventListener("scroll", checkLoadMore);
+    return () => container.removeEventListener("scroll", checkLoadMore);
+  });
+
   return (
     <div
       className={classNames(
-        "flex flex-row items-stretch h-40 space-x-0.5 overflow-x-auto",
+        "relative flex flex-row items-stretch h-40 space-x-0.5 overflow-x-auto",
         className
       )}
+      ref={containerRef}
     >
-      {images.map((src, index) => (
-        <img
+      {images.slice(0, renderCount).map((src, index) => (
+        <CSSTransition
+          in={index < visibleCount}
           key={src}
-          src={src}
-          alt={src}
-          className="max-h-full cursor-pointer hover:opacity-90 transition-opacity"
-          onClick={(event) => {
-            setFullScreenImageIndex(index);
-            setIsImageOverlayOpen(true);
-            const clientRect = event.currentTarget.getBoundingClientRect();
-            setSourceImageRect(clientRect);
-          }}
-        />
+          timeout={150}
+          classNames={imageTransitionClassNames}
+        >
+          <img
+            className={styles.image}
+            onLoad={onImageLoad}
+            onError={onImageLoad}
+            onClick={(event) => {
+              setImageIndex(index);
+              setIsOverlayOpen(true);
+              const clientRect = event.currentTarget.getBoundingClientRect();
+              setSourceImageRect(clientRect);
+            }}
+            src={src}
+            alt={src}
+          />
+        </CSSTransition>
       ))}
+      {renderCount < images.length && (
+        <div className="w-40 shrink-0" ref={placeholderRef} />
+      )}
       <GalleryOverlay
         images={images}
-        index={fullScreenImageIndex}
-        onIndexChange={setFullScreenImageIndex}
-        visible={isImageOverlayOpen}
-        onClose={() => setIsImageOverlayOpen(false)}
+        index={imageIndex}
+        onIndexChange={setImageIndex}
+        visible={isOverlayOpen}
+        onClose={() => setIsOverlayOpen(false)}
         sourceRect={sourceImageRect}
       />
     </div>
