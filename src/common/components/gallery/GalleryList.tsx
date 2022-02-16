80fa0f8ca51cb88f170b88e71b80da749f83991e
Kimmo Kiiski <kimmo@kii.ski>
2022-02-16 13:43:32 +0200

    Gallery list is actually a row with line wrap

--- src/common/components/gallery/GalleryList.tsx
+++ src/common/components/gallery/GalleryList.tsx
@@ -11,13 +11,13 @@ const GalleryList: VFC<GalleryListProps> = ({ images }) => {
   const [isOverlayOpen, setIsOverlayOpen] = useState(false);
   const [sourceImageRect, setSourceImageRect] = useState<DOMRect | null>(null);
   return (
-    <div className="flex flex-col items-center space-y-1">
+    <div className="flex flex-row items-center justify-center flex-wrap gap-1">
       {images.map(({ src, title }, index) => (
         <img
           key={index}
           src={src}
           alt={title}
-          className="max-w-full max-h-80 cursor-pointer shadow hover:opacity-90 transition-opacity"
+          className="max-w-full max-h-80 shrink-0 cursor-pointer shadow hover:opacity-90 transition-opacity"
           onClick={(event) => {
             setImageIndex(index);
             setIsOverlayOpen(true);
