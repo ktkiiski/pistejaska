bf721b02f967f15fb89a10871cc21828addfb90e
Panu Vuorinen <panu.vuorinen@gmail.com>
2022-02-27 11:32:19 +0200

    Update CHANGELOG.MD

--- src/common/dateUtils.ts
+++ src/common/dateUtils.ts
@@ -4,6 +4,9 @@ const timeZone = Temporal.Now.timeZone();
 
 export const getTodayAsString = () => Temporal.Now.plainDateISO().toString();
 export const convertToLocaleDateString = (instant: Temporal.Instant) => {
+  if (!instant) {
+    return "";
+  }
   const zonedDateTime = instant.toZonedDateTimeISO(timeZone);
   const plainDate = Temporal.PlainDate.from(zonedDateTime);
 
@@ -11,6 +14,10 @@ export const convertToLocaleDateString = (instant: Temporal.Instant) => {
 };
 
 export const convertToLocaleTimeString = (instant: Temporal.Instant) => {
+  if (!instant) {
+    return "";
+  }
+
   const zonedDateTime = instant.toZonedDateTimeISO(timeZone);
   const plainTime = Temporal.PlainTime.from(zonedDateTime);
 
