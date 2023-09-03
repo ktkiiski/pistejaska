956f1eebd73a5a8a39c61ac46dd15cdfb83d5399
Markus Yrjölä <yrjola.markus@gmail.com>
2023-09-03 23:00:41 +0300

    Added a (dummy) view to the admin side for editing existing games
    The admin frontpage now has a dropdown for selecting an existing game for editing.
    
    Extracted the game edit form into its own component. Both the game creation and game editing views use the same form.
    
    The game edit view is currently just the game creation view with a different heading, the actual editing doesn't work yet.

--- src/admin/Admin.tsx
+++ src/admin/Admin.tsx
@@ -1,24 +1,48 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Button from "../common/components/buttons/Button";
 import ButtonPrimary from "../common/components/buttons/ButtonPrimary";
 import CardButtonRow from "../common/components/buttons/CardButtonRow";
 import Heading1 from "../common/components/typography/Heading1";
 import ViewContentLayout from "../common/components/ViewContentLayout";
+import { useGames } from "../common/hooks/useGames";
+import NativeSelectField from "../common/components/inputs/NativeSelectField";
 
 export default function Admin() {
   const navigate = useNavigate();
+  const [games] = useGames();
+  const [gameId, setGameId] = useState<string | null>(null);
 
   return (
     <ViewContentLayout>
       <Heading1>Add and edit games</Heading1>
       <CardButtonRow>
-        <ButtonPrimary onClick={() => navigate("/admin/edit-game")}>
+        <ButtonPrimary onClick={() => navigate("/admin/add-game")}>
           Add new game (beta)
         </ButtonPrimary>
         <Button onClick={() => navigate("/admin/edit-game-json")}>
           Game JSON Editor
         </Button>
       </CardButtonRow>
+
+      <div className="flex flex-row gap-2 items-center max-w-lg mx-auto mt-3">
+        <NativeSelectField
+          className="flex-1"
+          label="Edit existing game"
+          value={gameId}
+          onChange={setGameId}
+          options={games.map((game) => ({
+            value: game.id,
+            label: game.name || game.id,
+          }))}
+        />
+        <ButtonPrimary
+          onClick={() => navigate(`/admin/edit-game/${gameId}`)}
+          disabled={!gameId}
+        >
+          Edit game (beta)
+        </ButtonPrimary>
+      </div>
     </ViewContentLayout>
   );
 }
---
+++ src/admin/GameForm.tsx
@@ -0,0 +1,168 @@
+import { useNavigate } from "react-router-dom";
+import React, { FormEvent, useState } from "react";
+import EditGameBasicInfo from "./EditGameBasicInfo";
+import {
+  GameBasicInfoDefinition,
+  GameDefinition,
+  GameFieldOption,
+  GameMiscFieldDefinition,
+  GameScoreFieldDefinition,
+} from "../domain/game";
+import EditGameScoreField from "./EditGameScoreField";
+import Heading2 from "../common/components/typography/Heading2";
+import ButtonLight from "../common/components/buttons/ButtonLight";
+import { isEmpty, omit } from "lodash";
+import EditGameMiscField from "./EditGameMiscField";
+import ButtonPrimary from "../common/components/buttons/ButtonPrimary";
+import Spinner from "../common/components/Spinner";
+import { IconSmileyFace } from "../common/components/icons/IconSmileyFace";
+import classNames from "classnames";
+import saveGame from "../utils/saveGame";
+
+interface GameEditViewProps {
+  game?: GameDefinition;
+}
+
+interface KeyedScoreFields {
+  [key: string]: GameScoreFieldDefinition;
+}
+
+interface KeyedMiscFields {
+  [key: string]: GameMiscFieldDefinition;
+}
+
+function getDefaultBasicInfo(): GameBasicInfoDefinition {
+  return { id: "", name: "", icon: "", simultaneousTurns: false };
+}
+
+function getDefaultScoreField(): KeyedScoreFields {
+  return { [crypto.randomUUID()]: { id: "", name: "", type: "number" } };
+}
+
+function getDefaultMiscField(): KeyedMiscFields {
+  return { [crypto.randomUUID()]: { id: "", name: "", type: "text" } };
+}
+
+function omitEmptyOptions(
+  miscFields: GameMiscFieldDefinition[]
+): GameMiscFieldDefinition[] {
+  const isNotEmpty = (option: GameFieldOption<unknown>) =>
+    !isEmpty(option.label) || !isEmpty(option.value);
+  return miscFields.map((miscField) => ({
+    ...miscField,
+    options: miscField.options?.some(isNotEmpty)
+      ? miscField.options.filter(isNotEmpty)
+      : undefined,
+  }));
+}
+
+export default function GameForm({ game }: GameEditViewProps) {
+  const navigate = useNavigate();
+  const [basicInfo, setBasicInfo] = useState(getDefaultBasicInfo);
+  const [scoreFields, setScoreFields] = useState(getDefaultScoreField);
+  const [miscFields, setMiscFields] = useState<KeyedMiscFields>({});
+  const [isSaving, setIsSaving] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const onSave = async (event: FormEvent) => {
+    event.preventDefault();
+    if (isSaving || isSaved) return;
+    setIsSaving(true);
+    const game: GameDefinition = {
+      ...basicInfo,
+      scoreFields: Object.values(scoreFields),
+      miscFields: omitEmptyOptions(Object.values(miscFields)),
+    };
+
+    try {
+      await saveGame(game);
+      setIsSaving(false);
+      setIsSaved(true);
+      setTimeout(() => navigate("/admin"), 2000);
+    } catch (e) {
+      setIsSaving(false);
+      setIsSaved(false);
+      console.error(e);
+      alert(e);
+    }
+  };
+
+  return (
+    <form onSubmit={onSave}>
+      <fieldset disabled={isSaving || isSaved}>
+        <div className="flex flex-col items-center pb-2 space-y-1">
+          <EditGameBasicInfo
+            basicInfo={basicInfo}
+            onBasicInfoChange={(updatedBasicInfo) =>
+              setBasicInfo({ ...basicInfo, ...updatedBasicInfo })
+            }
+          />
+        </div>
+
+        <Heading2 className="mt-6">Score fields</Heading2>
+        <div className="flex flex-row flex-wrap gap-1.5 justify-center">
+          {Object.entries(scoreFields).map(([key, scoreField], i) => (
+            <EditGameScoreField
+              key={key}
+              scoreField={scoreField}
+              onScoreFieldChange={(updatedScoreField) =>
+                setScoreFields({ ...scoreFields, [key]: updatedScoreField })
+              }
+              onScoreFieldRemove={() => setScoreFields(omit(scoreFields, key))}
+              autoFocusFirstField={i !== 0}
+            />
+          ))}
+        </div>
+        <div className="text-center mt-3">
+          <ButtonLight
+            onClick={() =>
+              setScoreFields({ ...scoreFields, ...getDefaultScoreField() })
+            }
+          >
+            Add score field
+          </ButtonLight>
+        </div>
+
+        <Heading2 className="mt-6">Miscellaneous fields</Heading2>
+        <div className="flex flex-row flex-wrap gap-1.5 justify-center">
+          {Object.entries(miscFields).map(([key, miscField]) => (
+            <EditGameMiscField
+              key={key}
+              miscField={miscField}
+              onMiscFieldChange={(updatedMiscField) =>
+                setMiscFields({ ...miscFields, [key]: updatedMiscField })
+              }
+              onMiscFieldRemove={() => setMiscFields(omit(miscFields, key))}
+            />
+          ))}
+        </div>
+        <div className="text-center mt-3">
+          <ButtonLight
+            onClick={() =>
+              setMiscFields({ ...miscFields, ...getDefaultMiscField() })
+            }
+          >
+            Add miscellaneous field
+          </ButtonLight>
+        </div>
+
+        <div className="flex flex-col items-center my-6">
+          <ButtonPrimary
+            type="submit"
+            className={classNames({ "w-32": !isSaved })}
+          >
+            {!isSaving && !isSaved && "Save game"}
+            {isSaving && <Spinner className="inline-block w-5 h-5" />}
+            {isSaved && (
+              <>
+                <IconSmileyFace className="inline-block w-5 h-5 mr-2" />
+                Game saved!
+                <IconSmileyFace className="inline-block w-5 h-5 ml-2" />
+              </>
+            )}
+          </ButtonPrimary>
+        </div>
+      </fieldset>
+    </form>
+  );
+}
